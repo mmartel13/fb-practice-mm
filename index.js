3b1f056f0d19d0922df5b9d7e7be45f761aac278
Megan Martel <meganmartel@Megans-MacBook-Air.local>
2022-01-27 10:58:32 -0500

    imported restaurants.json

---
+++ index.js
@@ -0,0 +1,13 @@
+
+// import our restaurants 
+
+const restaurants = require('./restaurants.json');
+
+console.log(restaurants[0].name)
+
+// connect to Firestore 
+
+// create a collection called "restaurants"
+
+// add each restaurant 
+
